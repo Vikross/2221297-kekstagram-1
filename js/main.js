a34f7b3791b8442b1170cbd5dadc1cdb6a1bbc4c
Vikross <Viktoria.Emelkina@urfi.me>
2022-12-18 20:32:52 +0500

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 import './bigpict.js';
-import './util.js';
-import './pictures.js';
 import './newform.js';
 import './validator.js';
 import './sc.js';
 import './effects.js';
+import {renderPhotos} from'./pictures.js';
+import {sendNewRequest} from './fetch.js';
+
+let photos = [];
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(photos);
+};
+
+const onFail = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.textAlign = 'center';
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.padding = '10px 5px';
+  messageAlert.textContent = 'Ошибка загрузки данных';
+  document.body.append(messageAlert);
+};
+
+sendNewRequest(onSuccess, onFail, 'GET');
--- js/util.js
+++ js/util.js
@@ -1,6 +1,3 @@
-import { getRandomNumber } from './randnum.js';
-
-const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
 const isEscape = (evt) => evt.key === 'Escape';
 
-export {getRandomElement, isEscape};
+export {isEscape};
